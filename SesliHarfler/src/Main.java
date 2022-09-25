347fd2aecf195db4f6dd6b7db19de22385ba184f
ibrahim Alan <ibra.alann@gmail.com>
2022-09-25 10:17:21 +0300

    kod güncelleme

--- SesliHarfler/src/Main.java
+++ SesliHarfler/src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        char harf='A';
+        char harf='i';
 
-
-        switch (harf){
-            case 'A':
-            case 'I':
-            case 'O':
-            case 'U':
+        char harf2=Character.toLowerCase(harf);
+        switch (harf2){
+            case 'a':
+            case 'ı':
+            case 'o':
+            case 'u':
                 System.out.println("Kalın Sesli Harf");
                 break;
-            case 'E':
-            case 'İ':
-            case 'Ö':
-            case 'Ü':
+            case 'e':
+            case 'i':
+            case 'ö':
+            case 'ü':
                 System.out.println("İnce Sesli Harf");
                 break;
             default:
